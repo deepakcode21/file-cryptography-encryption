abb7d16977fc2a10589dce72ffb34f6cd8c42191
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-04-15 19:16:46 +0530

    ping for server

--- server/server.js
+++ server/server.js
@@ -24,6 +24,11 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use('/api', fileRoutes);
 
+// Ping 
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
